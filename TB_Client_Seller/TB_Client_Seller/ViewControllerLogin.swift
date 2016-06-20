fb3f18c8297deaae1b62f377bc8da69981d612eb
Bastian Gardel <bastiangardel@gmail.com>
2016-06-20 17:39:46 +0200

    update

--- TB_Client_Seller/TB_Client_Seller/ViewControllerLogin.swift
+++ TB_Client_Seller/TB_Client_Seller/ViewControllerLogin.swift
@@ -7,12 +7,53 @@
 //
 
 import UIKit
+import KeychainSwift
+import BButton
+import MBProgressHUD
+import SCLAlertView
 
 class ViewControllerLogin: UIViewController {
+   
+   
+   @IBOutlet weak var LoginTF: UITextField!
 
+   @IBOutlet weak var PasswordTF: UITextField!
+   
+   
+   @IBOutlet weak var SaveLP: UISwitch!
+   
+   @IBOutlet weak var loginButton: BButton!
+   
+   let keychain = KeychainSwift()
+   
+   var httpsSession = HTTPSSession.sharedInstance
+   
+   var hud: MBProgressHUD?
+   
+   override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+      LoginTF.endEditing(true)
+      PasswordTF.endEditing(true)
+      
+   }
+
+   
    override func viewDidLoad() {
       super.viewDidLoad()
-      // Do any additional setup after loading the view, typically from a nib.
+      
+      loginButton.color = UIColor.bb_successColorV2()
+      loginButton.setStyle(BButtonStyle.BootstrapV2)
+      loginButton.setType(BButtonType.Success)
+      loginButton.addAwesomeIcon(FAIcon.FASignIn, beforeTitle: false)
+      
+      
+      if ((keychain.get("login")) != nil && (keychain.get("password")) != nil) {
+         LoginTF.text = keychain.get("login");
+         PasswordTF.text = keychain.get("password")
+      }
+      
+      if keychain.getBool("SaveLP")  != nil {
+         SaveLP.setOn(keychain.getBool("SaveLP")!, animated: false)
+      }
    }
 
    override func didReceiveMemoryWarning() {
@@ -20,6 +61,24 @@ class ViewControllerLogin: UIViewController {
       // Dispose of any resources that can be recreated.
    }
 
+   @IBAction func loginAction(sender: AnyObject) {
+      super.viewDidLoad()
+      
+      loginButton.color = UIColor.bb_successColorV2()
+      loginButton.setStyle(BButtonStyle.BootstrapV2)
+      loginButton.setType(BButtonType.Success)
+      loginButton.addAwesomeIcon(FAIcon.FASignIn, beforeTitle: false)
+      
+      
+      if ((keychain.get("login")) != nil && (keychain.get("password")) != nil) {
+         LoginTF.text = keychain.get("login");
+         PasswordTF.text = keychain.get("password")
+      }
+      
+      if keychain.getBool("SaveLP")  != nil {
+         SaveLP.setOn(keychain.getBool("SaveLP")!, animated: false)
+      }
+   }
 
 }
 
