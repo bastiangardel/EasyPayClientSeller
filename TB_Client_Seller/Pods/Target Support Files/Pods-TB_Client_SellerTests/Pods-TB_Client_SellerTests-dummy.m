a124add4241c26097a829a65bdf736b67c9d4a73
Bastian Gardel <bastiangardel@gmail.com>
2016-06-20 16:59:37 +0200

    delete

--- TB_Client_Seller/Pods/Target Support Files/Pods-TB_Client_SellerTests/Pods-TB_Client_SellerTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_TB_Client_SellerTests : NSObject
-@end
-@implementation PodsDummy_Pods_TB_Client_SellerTests
-@end
--- TB_Client_Seller/Pods/Target Support Files/Pods-TB_Client_SellerTests/Pods-TB_Client_SellerTests-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_TB_Client_SellerTestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_TB_Client_SellerTestsVersionString[];
-
