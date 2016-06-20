d741ebcd9bae7f25d72a7a63368c14058f4393f1
Bastian Gardel <bastiangardel@gmail.com>
2016-06-20 18:01:12 +0200

    initial commit

---
+++ TB_Client_Seller/Pods/Target Support Files/Pods-TB_Client_Seller/Pods-TB_Client_Seller-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TB_Client_Seller : NSObject
+@end
+@implementation PodsDummy_Pods_TB_Client_Seller
+@end
---
+++ TB_Client_Seller/Pods/Target Support Files/Pods-TB_Client_SellerUITests/Pods-TB_Client_SellerUITests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_TB_Client_SellerUITestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_TB_Client_SellerUITestsVersionString[];
+
